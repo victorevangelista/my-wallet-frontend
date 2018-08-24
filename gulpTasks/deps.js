d3e5b17efe8bb04fcf9d5e0a6a12308846ee2193
Victor Evangelista <victorevangelista@gmail.com>
2018-08-24 15:37:32 -0300

    Run project gulp on Heroku

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -7,13 +7,13 @@ gulp.task('deps', ['deps.js', 'deps.css', 'deps.fonts'])
 
 gulp.task('deps.js', () => {
 	return gulp.src([
+	    'node_modules/admin-lte/plugins/JQuery/jquery-2.2.3.min.js',
+	    'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
 		'node_modules/angular/angular.min.js',
 	    'node_modules/angular-ui-router/release/angular-ui-router.min.js',
 	    'node_modules/angular-animate/angular-animate.min.js',
 	    'node_modules/angular-toastr/dist/angular-toastr.tpls.min.js',
-	    'node_modules/admin-lte/plugins/JQuery/jquery-2.2.3.min.js',
 	    'node_modules/admin-lte/bootstrap/js/bootstrap.min.js',
-	    'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min.js',
 	    'node_modules/admin-lte/dist/js/app.min.js'
 	])
 	.pipe(concat('deps.min.js'))
